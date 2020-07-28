65440920b5306a86fccb10408d19ccdd1d8378ad
Nico <nicoc@ovationsgroup.com>
2020-07-28 08:55:50 +0200

    Added HTTP Errors

--- src/sample/simple.py
+++ src/sample/simple.py
@@ -5,7 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 api = Api(app)
 
-VERSION = '2.0.0'
+VERSION = '3.0.0'
 
 class Probes:
     def __init__(self):
@@ -31,36 +31,44 @@ class VersionCheck(Resource):
             'timestamp': '{}'.format(datetime.now().isoformat()),
             'features': [
                 'Readiness can be toggled',
+                'Liveness can be toggled',
             ],
         }
 
 
 class Liveness(Resource):
     def get(self):
-        return {
-            'Alive': probes.liveness,
-        }
+        status = 410
+        if probes.liveness is True:
+            status = 200
+        return {'Alive': probes.liveness,}, status
 
 
 class Readiness(Resource):
     def get(self):
-        return {
-            'Ready': probes.readiness,
-        }
+        status = 410
+        if probes.readiness is True:
+            status = 200
+        return {'Ready': probes.readiness,}, status
 
 
 class ToggleReadiness(Resource):
     def get(self):
         probes.readiness = not probes.readiness
-        return {
-            'CommandStatus': 'Ok',
-        }
+        return {'CommandStatus': 'Ok',}
+
+
+class ToggleLiveness(Resource):
+    def get(self):
+        probes.liveness = not probes.liveness
+        return {'CommandStatus': 'Ok',}
 
 
 api.add_resource(VersionCheck, '/version')
 api.add_resource(Liveness, '/liveness')
 api.add_resource(Readiness, '/readiness')
 api.add_resource(ToggleReadiness, '/admin/readiness-toggle')
+api.add_resource(ToggleLiveness, '/admin/liveness-toggle')
 
 
 if __name__ == '__main__':
